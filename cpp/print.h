3f970a974875e8ee78444995fb04d8a5426f985d
artyoomi <zuodleq@gmail.com>
2025-08-03 12:03:56 +0300

    Remove some useless things

--- cpp/print.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-template <typename T>
-inline void print(const T *arr, std::size_t size)
-{
-    for (std::size_t i = 0; i < size; ++i)
-        std::cout << arr[i] << ((i == size - 1) ? ('\n') : (' '));
-}
